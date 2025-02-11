59369c11bfb491bc4899fffea30618f8525e3af4
ganesh siripuram <siripuramganesh4444@gmail.com>
2025-02-11 23:40:01 +0530

    testing

---
+++ src/test/java/com/ganeshsiripuram/springtesting/springtesting/SpringtestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganeshsiripuram.springtesting.springtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
